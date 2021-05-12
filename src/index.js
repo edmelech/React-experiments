443013dfc2cb5cbc520c9235ee0cc8df975cb840
Ed Melech <mr.melech@gmail.com>
2021-05-12 14:42:16 +0100

    replace 'class' with 'className'

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import ReactDOM from 'react-dom';
 const App = () => {
   return (
     <div>
-      <label class="label" for="name">Enter name:</label>
+      <label className="label" for="name">
+        Enter name:</label>
       <input id="name" type="text"/>
       <button style={{ backgroundColor: 'blue', color: 'white' }}>
         Submit
